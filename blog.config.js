8099875fb3d7851eb0b6626d0b1010f398b502c2
One <91908092+pikaOne1138@users.noreply.github.com>
2024-08-02 10:33:14 +0800

    Update blog.config.js
    微調

--- blog.config.js
+++ blog.config.js
@@ -19,14 +19,14 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，歡迎來到萬萬的伊甸園點子庫 🎉',
+    'Hi，歡迎來到萬萬的伊甸園',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '萬萬', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '萬萬就是萬萬', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://oneone-book.vercel.app', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '人類圖, Notion, 萬萬, 人類設計, 內在系統 ', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '萬萬, 人類圖, Notion, 人類設計, 內在系統 ', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
@@ -204,7 +204,7 @@ const BLOG = {
   POST_SHARE_BAR_ENABLE: process.env.NEXT_PUBLIC_POST_SHARE_BAR || 'true', // 文章分享功能 ，将在底部显示一个分享条
   POSTS_SHARE_SERVICES:
     process.env.NEXT_PUBLIC_POST_SHARE_SERVICES ||
-    'link,facebook,line,email,messenger,telegram,twitter,reddit,whatsapp,wechat,qq,weibo,linkedin', // 分享的服務，按顺序显示,逗号隔开
+    'link,facebook,line,email,messenger,telegram,twitter,reddit,whatsapp,wechat,linkedin', // 分享的服務，按顺序显示,逗号隔开
   // 所有支持的分享服务：link(复制链接),wechat(微信),qq,weibo(微博),email(邮件),facebook,twitter,telegram,messenger,line,reddit,whatsapp,linkedin,vkshare,okshare,tumblr,livejournal,mailru,viber,workplace,pocket,instapaper,hatena
 
   // 文章URL前缀
