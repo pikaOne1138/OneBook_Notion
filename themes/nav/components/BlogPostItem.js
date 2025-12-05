9f97b1e2dd3c801b2da0f5a7eb8d9cb25be5eb2b
One <91908092+pikaOne1138@users.noreply.github.com>
2025-12-05 16:19:26 +0800

    Add tagOptions prop to BlogPostCard component
    增加標籤屬性

--- themes/nav/components/BlogPostItem.js
+++ themes/nav/components/BlogPostItem.js
@@ -22,7 +22,7 @@ const BlogPostItem = (props) => {
             </div>
             <div id='posts-wrapper' className='card-list grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5' >
             {group?.items?.map(post => (
-                <BlogPostCard key={post.id} className='card' post={post} />
+                <BlogPostCard key={post.id} className='card' post={post} tagOptions={tagOptions} />
             ))}
             </div>
         </>
